f0e8a9e37b72d4c8d24ba288e58dff302da6eed4
N SRIDHAR SAI <140067828+Sridhar2803@users.noreply.github.com>
2025-03-25 15:37:46 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -6,13 +6,17 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
+
 df = pd.read_csv('AppleNewsStock.csv')
+
 
 df['Date'] = pd.to_datetime(df['Date'])
 df.set_index('Date', inplace=True)
 
+
 df['Returns'] = df['Adj Close'].pct_change()
 df.dropna(inplace=True)
+
 
 def analyze_sentiment(news_column):
     sentiments = []
@@ -26,23 +30,27 @@ def analyze_sentiment(news_column):
 
 df['Sentiment'] = analyze_sentiment(df['News'])
 
+
 X = df[['Sentiment']]
 y = df['Returns']
 
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
 
 model = RandomForestRegressor(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 
+
 predictions = model.predict(X_test)
 mse = mean_squared_error(y_test, predictions)
-print("Mean Squared Error:", mse)
+print("Mean Squared Error:",mse)
 
 future_sentiment = np.array([[0.8], [-0.2]])
 predicted_returns = model.predict(future_sentiment)
 print("Predicted stock returns:", predicted_returns)
 
-plt.scatter(y_test, predictions)
+plt.scatter(y_test,predictions)
 plt.xlabel("True Prices")
 plt.ylabel("Predictions")
 plt.show()
